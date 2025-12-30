724aaa69037e3de061a6ede63034d00ecb1971b1
Patricio.Garcia <garcia.patricio.andres@gmail.com>
2025-12-30 18:31:23 -0300

    Dec30_2025_18:31

--- Ejercitaciones/JS_Dec25_2025/script.js
+++ Ejercitaciones/JS_Dec25_2025/script.js
@@ -1,8 +1,31 @@
 let promedio = 0
 let notaTotal = 0
-let cantidadNotas = prompt('Ingrese un número que represente la cantidad de notas a promediar')
+let cantidadNotas = prompt('Ingrese un número que represente la cantidad de notas a promediar.')
 
-if (cantidadNotas && !isNaN(cantidadNotas) && Number(cantidadNotas) > 0) {
+while (!cantidadNotas || isNaN(cantidadNotas) || Number(cantidadNotas) <= 0) {
+    alert('Número ingresado no válido. Pruebe nuevamente.')
+    cantidadNotas = prompt('Ingrese un número que represente la cantidad de notas a promediar.')
+}
+
+cantidadNotas = Number(cantidadNotas)
+for (let i = 1; i <= cantidadNotas; i++) {
+    let nota = prompt('Ingrese la nota número ' + i)
+
+    while (!nota || isNaN(nota) || Number(nota) <= 0 || Number(nota) > 10) {
+        alert('Nota ingresada no válida. Pruebe nuevamente.')
+        nota = prompt('Ingrese la nota número ' + i + ':')
+    }
+
+    nota = Number(nota)
+    notaTotal = notaTotal + nota
+}
+
+promedio = notaTotal / cantidadNotas
+alert('El promedio de nota es: ' + promedio)
+
+
+
+/* if (cantidadNotas && !isNaN(cantidadNotas) && Number(cantidadNotas) > 0) {
     cantidadNotas = Number(cantidadNotas)
     for (let i = 1 ; i <= cantidadNotas ; i ++) {
         let nota = prompt('Ingrese la nota número ' + i + ':')
@@ -20,4 +43,4 @@ if (cantidadNotas && !isNaN(cantidadNotas) && Number(cantidadNotas) > 0) {
 else {
     alert('Número ingresado no válido.')
 }
-
+ */
